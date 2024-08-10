d4baf04a464df979211d6bf8b804d1e5f1acf6b3
Abdulmalik Alkhalifh <bdalmlk832@gmail.com>
2024-08-11 01:09:53 +0300

    complete quran page

---
+++ Quran-page/main.js
@@ -0,0 +1,40 @@
+
+
+
+function getSouras(){
+    axios.get("http://api.alquran.cloud/v1/quran/quran-uthmani")
+    .then((response) =>{
+        let souras = document.getElementById("souras")
+        let souraNames = response.data.data.surahs
+        for (souraName of souraNames){
+            souras.innerHTML += `<h1 class="soura" onclick="getAyat(${souraName.number})">${souraName.name}</h1>`
+        }
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+
+function getAyat(ayas){
+    axios.get(`http://api.alquran.cloud/v1/surah/${ayas}/quran-uthmani`)
+    .then((response)=>{
+
+        let ayat = document.getElementById("ayat-p")
+        let souraNameH1 = document.getElementById("soura-name-h1")
+
+        let soura = response.data.data
+        let souras = response.data.data.ayahs
+        ayat.innerHTML = ""
+        souraNameH1.innerHTML = `<h1>${soura.name}</h1>`
+        for (aya of souras){
+            ayat.innerHTML += `${aya.text}<span id="aya-number">{${aya.numberInSurah}}</span>`
+
+        }
+        ayat.innerHTML += "."
+        
+        //let sourahs = response.data.data.surahs.ayahs
+        
+    })
+}
+getAyat(1)
+getSouras()
